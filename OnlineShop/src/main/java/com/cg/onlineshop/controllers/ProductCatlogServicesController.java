5aec1d8612390aa6710b9509b6b2e10546f9cdab
Satmahaj <Satmahaj@LIN51005544.corp.capgemini.com>
2017-08-16 16:41:47 +0530

    updated

--- OnlineShop/src/main/java/com/cg/onlineshop/controllers/ProductCatlogServicesController.java
+++ OnlineShop/src/main/java/com/cg/onlineshop/controllers/ProductCatlogServicesController.java
@@ -2,8 +2,6 @@ package com.cg.onlineshop.controllers;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
