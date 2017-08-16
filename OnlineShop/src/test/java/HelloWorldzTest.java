1d74f6e139dc058f61d7f498421c2b28e9d83500
Satmahaj <Satmahaj@LIN51005544.corp.capgemini.com>
2017-08-16 14:36:11 +0530

    Removed Commit

--- OnlineShop/src/test/java/HelloWorldzTest.java
+++ OnlineShop/src/test/java/HelloWorldzTest.java
@@ -1,11 +1,8 @@
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.internal.runners.TestMethod;
 
 public class HelloWorldzTest {
 
